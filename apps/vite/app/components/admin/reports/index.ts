78dfd6078da1e65595d5f5a4d7f392b967c64cb7
panjiggm <panjiggm@gmail.com>
2025-06-22 22:15:35 +0700

    add new handlers

---
+++ apps/vite/app/components/admin/reports/index.ts
@@ -0,0 +1,11 @@
+export { TabIndividual } from "./TabIndividual";
+export { TabSession } from "./TabSession";
+export { EmptyDetailView } from "./EmptyDetailView";
+export { IndividualDetailView } from "./IndividualDetailView";
+export { SessionDetailView } from "./SessionDetailView";
+
+// Re-export types
+export type {
+  IndividualReportsListItem,
+  SessionReportsListItem,
+} from "./types";
